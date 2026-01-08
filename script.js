8e1abb570837a7d8c422af9b983275d8f1475326
nadiairina <nadiairina934@gmail.com>
2026-01-08 23:38:36 +0000

    Update script.js

--- script.js
+++ script.js
@@ -106,3 +106,12 @@ document.querySelectorAll('.nav-links a').forEach(link => {
         document.body.style.overflow = 'auto';
     });
 });
+const menuIcon = document.getElementById('menu-icon');
+const navLinks = document.getElementById('nav-links');
+
+if (menuIcon) {
+    menuIcon.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+        menuIcon.classList.toggle('active');
+    });
+}
